26d83806d049b4f593d23195610378a7e4c3d208
monerapersian <monerapersian@gmail.com>
2025-10-27 04:42:14 +0330

    Call Us LINK & URL commmit

--- website/urls.py
+++ website/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('products/<slug:slug>/', views.product_detail, name='product_detail'),
     path('articles', views.articles_list, name='articles_list'),
     path('articles/<slug:slug>/', views.article_detail, name='article_detail'),
-    path('callus', views.call_us, name='call_us'),
+    path('call_us', views.call_us, name='call_us'),
 ]
