ca6779ca971e7476f3664aa9884f3072fa1bb641
monerapersian <monerapersian@gmail.com>
2025-10-27 04:31:11 +0330

    Call Us URL commmit

--- website/urls.py
+++ website/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/category/<slug:slug>/', views.category_product, name='category_product'),
-    path('/products', views.products_list, name='products_list'),
-    path('/products/<slug:slug>/', views.product_detail, name='product_detail'),
-    path('/articles', views.articles_list, name='articles_list'),
-    path('/articles/<slug:slug>/', views.article_detail, name='article_detail'),
-    path('/callus', views.call_us, name='call_us'),
+    path('category/<slug:slug>/', views.category_product, name='category_product'),
+    path('products', views.products_list, name='products_list'),
+    path('products/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('articles', views.articles_list, name='articles_list'),
+    path('articles/<slug:slug>/', views.article_detail, name='article_detail'),
+    path('callus', views.call_us, name='call_us'),
 ]
