b56d310023078be046a9d336f21e2ffa77c90b2a
monerapersian <monerapersian@gmail.com>
2025-11-11 06:07:19 +0330

    Urls set again

--- farris_ir/settings.py
+++ farris_ir/settings.py
@@ -142,3 +142,5 @@ STATIC_ROOT = "/home/farrisir1/farris_ir/staticfiles/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+APPEND_SLASH = True
