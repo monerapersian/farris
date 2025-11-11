12a7fb892a544b322b00074cec30de10665a12d0
monerapersian <monerapersian@gmail.com>
2025-11-11 07:23:11 +0330

    Urls Regex

--- farris_ir/urls.py
+++ farris_ir/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    re_path(r'^admin(/.*)?$', admin.site.urls),
+    re_path(r'^admin/', admin.site.urls),
     path('', include('website.urls')),
 ] 
 
